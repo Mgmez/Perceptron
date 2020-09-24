bcde6fd3acd767e260fcd22bdc606b23688d371a
Antonio <thetonybl@gmailc.om>
2020-09-24 00:55:29 -0500

    Ajuste de w esta mal :vv

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,16 +5,35 @@ import Perceptron from '../hoocks/Perceptron';
 
 function App() { 
 
+  const [salida, setSalida ]= useState("");
+  var perceptron;
   
-  const perceptron = new Perceptron(10);
+
+  const iniciarPesos = () =>{
+     perceptron = new Perceptron(4, 0.01, 30);
+  }
+  const entrenar = () =>{
+    perceptron.fit();
+    console.log("xdxd");
+
+    console.log(perceptron.w);
+  }
+  const probar = () =>{
+    const x = [[3, 3]];
+    setSalida(perceptron.predict(x));
+  }
 
   return (
     <div className="App">
       <header className="App-header">        
         <p>Perceptrón </p>
 
-        <input type="submit" value="Submit" onClick  = {perceptron.fit}/>
+        <input type="submit" value="INiciar" onClick  = {iniciarPesos}/>
+        
+        <input type="submit" value="Entenar" onClick  = {entrenar}/>
 
+        <input type="submit" value="Probar" onClick  = {probar}/>
+        <a >{salida}</a>
       </header>
     </div>
   );
--- frontend/src/hoocks/Perceptron.js
+++ frontend/src/hoocks/Perceptron.js
@@ -1,48 +1,67 @@
-import { fireEvent } from "@testing-library/react";
+
 
 class  Perceptron {
   
-    constructor(size, learningRate, iterations){
+    constructor(size, lr, it){
 		this.w  = [];
-		this.lr = learningRate || 0.01;
-        this.it = iterations || 10;
+		this.learningRate=  lr || 0.01;
+        this.iterations =  it || 10;
             
         
-        for(var i = 0; i<size+1; i++){
+        for(var i = 0; i<size+1  ; i++){
             this.w[i] = Math.random() * (5 - (-5)) + (-5);
-        }        
-        console.log(this.w);  
+        }
+        console.log(this.w);
 	}
 
    
-    fit = (x,y) =>{
+
+    fit = (inputs, outputs) =>{
+   
+        const x = inputs || [
+            [5, 3, 2],
+            [4, 1, 3],
+            [3, 2, 3],
+            [3, 3, 3]
+        ];
+        const y = outputs || [0,0,1,1];
+
     
-        console.log(this.w);  
         var done = false;    
         var error = 0;
         var epoca = 0;
-        while(!done || epoca<this.iterations){
+        
+
+        while(done === false){
             done = true;
             //Epocas
-            console.log("Epoca: ", epoca +1);
-            for(var j = 0; j<x.length;j++){
+            console.log("Epoca: ", epoca+1);
+            for(var j = 0; j<x.length; j++){
                 error = y[j] - this.predict(x[j]);
                 console.log("error: ", error);
-                if(error !==0){
+                if(error!=0){
                     done = false;
-                    this.w[j] += this.learningRate  * error * this.x[j];
+                    this.w[j] += this.learningRate  * error * x[j];
                 }
-            }    
+            }
+            epoca += 1;   
+            if(epoca >=5){
+                break;
+            } 
         }
 
     }
 
     predict = (inputs) => {
-        var activation = 0;
+            let activation = 0;
+            console.log("x[j]: ", inputs);
+            console.log("Pesos ", this.w);
             for(var i = 0; i < inputs.length; i++){
                 activation += this.w[i+1] * inputs[i];
             }
-            return activation >= 0 ? 1 : 0;
+            const k = activation  >= 0 ? 1 : 0;
+            return k;
+            
         }
 
     
