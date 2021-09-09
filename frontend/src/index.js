bafc6d297ce3cce1d9ec420def51ed9ab85f62d3
mgmez <jmiguel.aguayo@alumnos.udg.mx>
2021-09-09 08:23:59 -0500

    New changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App.js'; 
+import App from './components/App.js';
 
 import * as serviceWorker from './serviceWorker';
 
@@ -10,6 +10,7 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+
 );
 
 // If you want your app to work offline and load faster, you can change
